b2b29120737f9955923d0b93ad2b307ac4dc0ea3
Kamakshi <kamakshipandoh8@gmail.com>
2024-04-04 11:43:50 +0530

    added put

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -3,13 +3,24 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-//route for post
+
 router.post('/users', async (req, res) => {
     try {
         const { username, password, email } = req.body;
         const user = new User({ username, password, email });
         await user.save();
         res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.put('/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { username, password, email } = req.body;
+        const updatedUser = await User.findByIdAndUpdate(id, { username, password, email }, { new: true });
+        res.json(updatedUser);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
