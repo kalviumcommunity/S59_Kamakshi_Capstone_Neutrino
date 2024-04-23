2a2c0c702d1950a6be75d2e1fa46c5588389c4fb
Kamakshi <kamakshipandoh8@gmail.com>
2024-04-23 17:41:26 +0530

    changes in ui component

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,12 +4,13 @@ import Registration from './pages/registration.jsx';
 import './tailwind.css';
 import Login from './pages/login.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GlobeDemo } from './components/ui/globeDemo.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <Routes>
-      <Route exact path="/" element={<Registration />} />
+      {/*<Route exact path="/" element={<Registration />} />
       <Route path="/login" element={<Login />} />
-    </Routes>
+*/}
+<GlobeDemo />
   </Router>
 );
