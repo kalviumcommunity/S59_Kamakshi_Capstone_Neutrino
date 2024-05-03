e5cebb26a22216e868316917db6fd007323d4bf6
Kamakshi <kamakshipandoh8@gmail.com>
2024-05-03 15:58:56 +0530

    added chnages to ui

--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { GlobeDemo } from '../components/ui/globeDemo.jsx';
 import { HeroParallaxDemo } from '../components/ui/heroDemo.jsx';
+import Navbar from './NavBar.jsx';
+import Footer from './FooterMain.jsx'
 
 export default function LandingPage() {
   return (
     <div>
+      <Navbar />
       <GlobeDemo />
       <HeroParallaxDemo />
+      <Footer />
     </div>
   );
 }
