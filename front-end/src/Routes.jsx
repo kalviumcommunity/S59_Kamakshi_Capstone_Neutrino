a6ddf3b04de345d395408bd9a13c9b8bb8ae9ef5
Kamakshi <kamakshipandoh8@gmail.com>
2024-05-22 11:35:29 +0530

    get api used

--- front-end/src/Routes.jsx
+++ front-end/src/Routes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage.jsx';
 import Registration from './pages/form/registration.jsx'; 
+import NewsPage from './components/main/NewPage.jsx';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/registration" element={<Registration />} />
+      <Route path="/news/:category" element={<NewsPage />} />
     </Routes>
   );
 }
