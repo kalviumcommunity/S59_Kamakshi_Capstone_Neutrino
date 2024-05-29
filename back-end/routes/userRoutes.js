56fdc71fe72d7895e4ebe426db993d4c1bceb60e
Kamakshi <kamakshipandoh8@gmail.com>
2024-05-29 18:13:57 +0530

    changes in schema

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -3,18 +3,16 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-
 router.post('/users', async (req, res) => {
     try {
-        const { first_name, last_name, email, password, username, marketing_accept } = req.body;
-        const user = new User({ first_name, last_name, email, password, username, marketing_accept });
+        const { email, password, username } = req.body;
+        const user = new User({ email, password, username });
         await user.save();
         res.status(201).json(user);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
 });
-
 
 router.put('/users/:id', async (req, res) => {
     try {
